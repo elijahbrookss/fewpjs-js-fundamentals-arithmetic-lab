2ea02f4d81adf7f71cf46f86dcbb0efdb3414567
elijahbrookss <elijahcbrooks@gmail.com>
2020-11-18 15:41:05 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+// elijah brooks
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1 * num2;
+let random = Math.random()*10;
+let mod = 16 % 6;
+let max = (14, 16, 13, 2, 20);
